22a714833f39f9824afef1d4c8120692ad5483fa
ngoducanh0505 <68346518+ngoducanh0505@users.noreply.github.com>
2020-08-08 00:43:26 +0700

    Initial commit

---
+++ Grid.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+
+export (int) var width;
+export (int) var height;
+export (int) var x_start;
+export (int) var y_start;
+export (int) var offset;
+
+export (PackedScene) var X_Icon
+
+var game_array = [];
+
+func _ready():
+	game_array = make_array();
+	print(game_array);
+
+
+func make_array():
+	var array = [];
+	for  i in width:
+		array.append([]);
+		for j in height:
+			array[i].append(null);
+	return array;
+
